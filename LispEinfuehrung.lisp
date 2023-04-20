bacde014dfcfb6627e4768aa94ab9b3208699918
Zemux1613 <42244022+Zemux1613@users.noreply.github.com>
2023-04-20 20:06:22 +0200

    feat: add comments

--- LispEinfuehrung.lisp
+++ LispEinfuehrung.lisp
@@ -1,11 +1,11 @@
-(setq x 3)
-(setq y (+ x (* x x)))
-(print (+ y x))
+(setq x 3)                                   ; x = 3
+(setq y (+ x (* x x)))                       ; y = x + (x*x)
+(print (+ y x))                              ; print x + y
 
-(defun quad (z) (* z z))
-(print (quad 2))
-(print (quad x))
-(print (quad y))
-(print (quad (* x x)))
-(print (quad (+ y y)))
-(print (quad (+ (* x x) y x x x x)))
+(defun quad (z) (* z z))                     ; define a function named quad with the argument "z" and the return value "z*z".
+(print (quad 2))                             ; call function quad with 2 as argument
+(print (quad x))                             ; call function quad with variable as argument
+(print (quad y))                             ; call function quad with variable as argument
+(print (quad (* x x)))                       ; call function quad with x*x as argument
+(print (quad (+ y y)))                       ; call function quad with y+y as argument
+(print (quad (+ (* x x) y x x x x)))         ; call function quad with (x*x)+y+x+x+x+x as argument
