07ed126d72301d0aad19544159daa0eb056d5248
Tom L <devzemux1613@gmail.com>
2023-06-21 19:58:45 +0200

    feat: add if grammars

---
+++ grammar/Tut_Thi1_12.py
@@ -0,0 +1,27 @@
+from nltk import CFG, ChartParser
+from nltk.parse.generate import generate
+
+if __name__ == '__main__':
+    IFGrammar = CFG.fromstring("""Start -> 'if(exp){' N1 '}' 
+                                N1    -> 'statement' | Start 
+                             """)
+
+    IfElseGrammar = CFG.fromstring("""Start -> 'if(exp){' N1 '}else{' N1 '}' |'if(exp){' N1 '}'  
+                                N1    -> 'statement' | Start 
+                             """)
+    IfElseIfElseGrammar = CFG.fromstring("""Start -> 'if(exp){' N1 '}' N2  
+                                N1    -> 'statement' | Start 
+                                N2 -> '' | 'else' Start | 'else {' N1 '}'
+                             """)
+
+    grammarInUse = IfElseGrammar
+
+    for item in list(generate(grammarInUse, depth=6)):
+        parser = ChartParser(grammarInUse)
+        parse = parser.parse(item)
+        for tree in parse:
+            tree.pretty_print()
+            tree.draw()
+            print(f"Height: {tree.height()}")
+            print(f"Leaves: {tree.leaves()}")
+
